7cc3ea981488e531dde0b14283f7b062ae28b8b7
Paul C <pcutter@cnm.edu>
2021-01-18 16:35:54 -0700

    implemented speed functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 class Bicycle {
   int cadence;
-  int speed;
   int gear;
+  int _speed = 0;
+  int get speed => _speed;
 
-  Bicycle(this.cadence, this.speed, this.gear);
+  Bicycle(this.cadence, this.gear);
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
 
   @override
-  String toString() => 'Bicycle: $speed mph';
-
+  String toString() => 'Bicycle: $_speed mph';
 }
 
 void main() {
-  var bike = Bicycle(2, 0, 1);
+  var bike = Bicycle(2, 1);
   print(bike);
 }
